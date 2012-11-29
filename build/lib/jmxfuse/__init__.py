3ba833d9dfcbbf2e971a2dff5a33d14daa7aabcf
alastair.mccormack <alastair.mccormack@gmail.com>
2012-11-29 22:14:53 +0000

    Removed build and dist dirs from Trunk

--- build/lib/jmxfuse/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    Expose JMX MBeans as a Fuse Filesystem
-    
-    @license: GPL
-    @copyright: Alastair McCormack
-    @author: Alastair McCormack
-    @contact: alastair@mcc-net.co.uk
-"""
